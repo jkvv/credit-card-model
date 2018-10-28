472058046737b008577ddf9c0a6275f721f8187e
Jakub Piasecki <j4kubpiasecki@gmail.com>
2018-10-28 11:22:23 +0000

    initialize maven archetype

---
+++ credit-card/src/main/java/pl/jpia/credit/App.java
@@ -0,0 +1,13 @@
+package pl.jpia.credit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
